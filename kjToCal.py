cff3319d493919415e5c580c1c2736cdb47cb4b7
Nick Walters <nicho.walters@gmail.com>
2020-10-10 19:25:55 +0800

    Update kjToCal.py

--- kjToCal.py
+++ kjToCal.py
@@ -1,4 +1,5 @@
 foodCount = int(input("How many items of food did you eat today? :  "))
+gender = str(input("What is your Gender? M or F ? : "))
 foodList = []
 foodListCalories = []
 
@@ -9,10 +10,36 @@ for food in range(foodCount):
 for item in foodList:
     foodListCalories.append(item * 0.238845897)
 
+
+netLossGain = 0
+    # gender is male (kj)
+if(gender.find("M") >= 0 or gender.find("m") >= 0):
+    netLossGain = 7100
+else:
+    # gender is female (kj)
+    netLossGain = 5900
+
+
 totalCalories = sum(foodListCalories)
+totalKg = (round(totalCalories) / 7700)
+print("\n\n\n")
+print("______________________________________________________")
 print("Total Calories Consumed: ")
-print("________________________")
-print("\n\n")
-print(totalCalories)
-print("\n\n")
-print("________________________")
+print("______________________________________________________")
+print("\n")
+print("Calories: ")
+print(round(totalCalories))
+print()
+print("Kg: ")
+print(round(totalKg, 3))
+print("\n")
+print("Net Loss/Gain: ")
+print("calculated based on basal metabolic rate of the average male/female \n The human body consumes a certain amount of calories/energy per day")
+print("______________")
+nlgCalories = netLossGain * 0.238845897
+nlgKg = (round(nlgCalories) / 7700)
+print("Calories: " + str(totalCalories - nlgCalories))
+print("Kg: " + str(totalKg - nlgKg))
+print()
+print("______________________________________________________")
+print("______________________________________________________")
